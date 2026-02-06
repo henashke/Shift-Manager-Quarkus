5e026fab459d5ede6c9eb3e663bce3f1ebf51557
YaaraWeill <yaara.weill@gmail.com>
2026-02-06 14:09:16 +0200

    added example properties file

--- src/main/java/services/ShiftService.java
+++ src/main/java/services/ShiftService.java
@@ -7,8 +7,12 @@ import daos.BaseDao;
 import entities.AssignedShift;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import mappers.AssignedShiftMapper;
 import mappers.CommandToEntityMapper;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @ApplicationScoped
 public class ShiftService extends BaseService<AssignedShift, AddShiftCommand, UpdateShiftCommand> {
@@ -27,5 +31,15 @@ public class ShiftService extends BaseService<AssignedShift, AddShiftCommand, Up
     @Override
     protected CommandToEntityMapper<AssignedShift, AddShiftCommand, UpdateShiftCommand> getMapper() {
         return assignedShiftMapper;
+    }
+
+    @Transactional
+    public void deleteShiftsForWeek(LocalDate weekStart) {
+        LocalDate weekEnd = weekStart.plusDays(6);
+        assignedShiftDao.delete("date >= ?1 and date <= ?2", weekStart, weekEnd);
+    }
+
+    public List<AssignedShift> getAllShiftsBetween(LocalDate startDate, LocalDate endDate) {
+        return assignedShiftDao.find("date >= ?1 and date <= ?2", startDate, endDate).list();
     }
 }
